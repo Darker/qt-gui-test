68814e535ab22cd20fc83ad85b02000112205346
Jakub Mareda <mareda@techsys.cz>
2016-08-31 18:25:47 +0200

    Reloading commands automatically from file now.

---
+++ client/nodejs/CommandLoader.js
@@ -0,0 +1,47 @@
+var Promise = require('bluebird');
+const Command = require("./Command");
+const fs = require("fs");
+fs.statPromise = Promise.promisify(fs.stat);
+fs.readFilePromise = Promise.promisify(fs.readFile);
+fs.writeFilePromise = Promise.promisify(fs.writeFile);
+
+function CommandLoader(file) {
+    this.lastUpdate = 0;
+    this.path = file;
+    this.cache = [];
+}
+CommandLoader.prototype.getCommands = function() {
+    return fs.statPromise(this.path)
+      .then((stat)=>{
+          if(!stat.isFile()) {
+              throw new Error("File "+this.path+" does not exist or is not a file!");
+          }
+          var time = this.lastUpdate;
+          var mtime = stat.mtime.getTime();
+          console.log("Getting commands");
+          if(time < mtime) {
+              console.log("  ... from disk.");
+              return this.loadCommands();
+          }
+          else {
+              console.log("  ... from cache.");
+              return this.cache;
+          }
+      })
+      .catch(function(error) {
+           console.log("Error in commands: "+error);
+           return this.cache;
+      }.bind(this));
+}
+CommandLoader.prototype.loadCommands = function() {
+    return fs.readFilePromise(this.path)
+      .then((data) => {
+           this.lastUpdate = new Date().getTime();
+           var commands = eval("("+data+")");
+           if(!(commands instanceof Array))
+               throw new error("Provide file with an array of commands!");
+           return this.cache = new Command.List(commands);
+      });
+}
+module.exports = CommandLoader;
+
---
+++ client/nodejs/ReloadTest.js
@@ -0,0 +1,37 @@
+var connect = require('net');
+var Promise = require('bluebird');
+const CommandLoader = require("./CommandLoader");
+// 
+function connectForever(port, ip, commands) {
+    if(typeof commands=="string")
+        commands = new CommandLoader(commands);
+    //var getCommands = commands.getCommands();
+
+    var client = connect.connect(port, ip);
+
+    client.on('data', function (data) {
+        // Log the response from the HTTP server.
+        console.log('RECEIVED: ' + data);
+    }).on('connect', function () {
+        console.log('Connection Success!\n\n');
+        commands.getCommands().then(function(commands) {
+            commands.execute(client)
+              .then(function () {
+                  console.log("All commands done!");
+              })
+              .catch(function (error) {
+                  console.log("Error while executing commands: " + error);
+              })
+        });
+    }).on('end', function () {
+        console.log('Disconnected');
+        connectForever(port, ip, commands);
+    }).on('error', function (error) {
+        if(error.code!="ECONNREFUSED") {
+            console.log("ERROR: " + JSON.stringify(error));
+        }
+        connectForever(port, ip, commands);
+    });
+}
+//connectForever("9666", "127.0.0.1", commands);
+module.exports = connectForever;
